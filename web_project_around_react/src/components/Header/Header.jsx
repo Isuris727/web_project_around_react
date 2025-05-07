6c440dd62a13b6cba18296a0ce93c738519eab92
Isuris <ajudithsalasr@gmail.com>
2025-05-07 00:08:44 -0600

    feat(App) add blocks, images and first components

---
+++ web_project_around_react/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header className="header page__header">
+      <img
+        className="header__logo"
+        alt="Logo Around the U.S."
+        src="./../images/Vector_around.png"
+      />
+    </header>
+  );
+}
+
+export default Header;
