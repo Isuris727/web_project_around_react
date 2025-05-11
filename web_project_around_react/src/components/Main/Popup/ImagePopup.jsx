0b9f285189342b9fd859380315fddf0fbc41cd3a
Isuris <ajudithsalasr@gmail.com>
2025-05-11 01:10:03 -0600

    fix(popup) Correct ImagePopup

--- web_project_around_react/src/components/Main/Popup/ImagePopup.jsx
+++ web_project_around_react/src/components/Main/Popup/ImagePopup.jsx
@@ -2,10 +2,10 @@ function ImagePopup(props) {
   const { name, link } = props;
 
   return (
-    <>
+    <div className="popup__img-container">
       <img className="popup__img" alt={name} src={link} />
       <p className="popup__card-name">{name}</p>
-    </>
+    </div>
   );
 }
 
